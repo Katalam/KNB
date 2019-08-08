4f60f7ec263af85efdc985db3afefb677e9f4979
Katalam <goerss99@gmail.com>
2019-08-08 19:46:03 +0200

    ADDED font

---
+++ addons/font/CfgFontFamilies.hpp
@@ -0,0 +1,37 @@
+class CfgFontFamilies {
+    class manic {
+    	fonts[] = {
+            QPATHTOF(manic\manic-depressive6),
+            QPATHTOF(manic\manic-depressive7),
+            QPATHTOF(manic\manic-depressive8),
+            QPATHTOF(manic\manic-depressive9),
+            QPATHTOF(manic\manic-depressive10),
+            QPATHTOF(manic\manic-depressive11),
+            QPATHTOF(manic\manic-depressive12),
+            QPATHTOF(manic\manic-depressive13),
+            QPATHTOF(manic\manic-depressive14),
+            QPATHTOF(manic\manic-depressive15),
+            QPATHTOF(manic\manic-depressive16),
+            QPATHTOF(manic\manic-depressive17),
+            QPATHTOF(manic\manic-depressive18),
+            QPATHTOF(manic\manic-depressive19),
+            QPATHTOF(manic\manic-depressive20),
+            QPATHTOF(manic\manic-depressive21),
+            QPATHTOF(manic\manic-depressive22),
+            QPATHTOF(manic\manic-depressive23),
+            QPATHTOF(manic\manic-depressive24),
+            QPATHTOF(manic\manic-depressive25),
+            QPATHTOF(manic\manic-depressive26),
+            QPATHTOF(manic\manic-depressive27),
+            QPATHTOF(manic\manic-depressive28),
+            QPATHTOF(manic\manic-depressive29),
+            QPATHTOF(manic\manic-depressive30),
+            QPATHTOF(manic\manic-depressive31),
+            QPATHTOF(manic\manic-depressive34),
+            QPATHTOF(manic\manic-depressive35),
+            QPATHTOF(manic\manic-depressive46)
+        };
+    	spaceWidth = 0.7;
+    	spacing = 0.13;
+    };
+};
---
+++ addons/font/config.cpp
@@ -0,0 +1,14 @@
+#include "script_component.hpp"
+    class CfgPatches {
+        class ADDON {
+            name = COMPONENT;
+            units[] = {};
+            weapons[] = {};
+            requiredVersion = REQUIRED_VERSION;
+            requiredAddons[] = {};
+            author = "Katalam";
+            VERSION_CONFIG;
+        };
+    };
+
+#include "CfgFontFamilies.hpp"
