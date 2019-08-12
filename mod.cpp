ff003d2905946613dd0abeb591ccdbf312edfdeb
Katalam <goerss99@gmail.com>
2019-08-12 21:44:34 +0200

    Added mod logo

--- mod.cpp
+++ mod.cpp
@@ -1,3 +1,9 @@
 name = "KAT Notebook";
 dir = "@KNB";
 author = "Katalam";
+picture = "book.paa";
+logo = "book.paa";
+logoOver = "book.paa";
+overviewPicture = "book.paa";
+actionName = "Github";
+action = "https://github.com/Katalam/KNB";
